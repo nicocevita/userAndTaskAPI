6c1bb53ba2b709d9774dcba2701d1f5ce896d233
Nicolas <nicolasevarela@gmail.com>
2022-02-09 14:07:19 -0300

    configurate cors

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const express = require('express');
 const connectDB = require('./database/db')
+const cors = require('cors')
+
+//cors configuration
+const whitelist = ["http://localhost:3000"]
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
+  credentials: true,
+}
+app.use(cors(corsOptions))
 
 //create server
 const app = express();
