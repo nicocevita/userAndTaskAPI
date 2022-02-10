dedea3f0c71dcfc7c6df3e6a11162acab5816635
Nicolas <nicolasevarela@gmail.com>
2022-02-10 01:31:03 -0300

    change type of completed on task response

--- models/Task.js
+++ models/Task.js
@@ -4,7 +4,7 @@ const TaskSchema = mongoose.Schema({
     user_id: {type: Number, required: true},
     id: {type: Number, required: true},
     title: {type: String, required: true},
-    completed: {type: String, required: true},
+    completed: {type: Boolean, required: true},
 })
 
 module.exports = mongoose.model('Task', TaskSchema);
